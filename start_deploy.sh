10e5324bfe8528b9754e5dc0e7719c125c44c4be
feenord <feenord40@gmail.com>
2022-05-16 00:47:40 +0300

    add .git ignore

--- start_deploy.sh
+++ start_deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-git pull --recurse-submodules
+git submodule update --remote --merge
 cd search_engine_crawler/
 scripts/build.sh
 cd ../
@@ -8,3 +8,6 @@ scripts/build.sh
 cd ../
 cd monitoring/
 scripts/build.sh
+cd ../
+yc compute instance list |grep monitoring
+yc compute instance list |grep crawler
